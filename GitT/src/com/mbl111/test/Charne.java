238261c50d8aa15937a2d3018fa103e1ef7c5b48
mbl111 <matt@mbl111.com>
2012-11-06 19:02:21 +1100

    Hey You

--- GitT/src/com/mbl111/test/Charne.java
+++ GitT/src/com/mbl111/test/Charne.java
@@ -2,7 +2,7 @@ package com.mbl111.test;
 
 public class Charne {
 
-	public static String s = "Hey";
+	public static String s = "Hey You";
 	
 	public static void main(String[] args) {
 		System.out.println(s);
