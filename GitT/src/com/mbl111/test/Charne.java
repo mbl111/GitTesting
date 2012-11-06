1275b9c9d6abde58eeb180c2e0758c80d899ebfe
mbl111 <matt@mbl111.com>
2012-11-06 18:39:30 +1100

    Let the message be printed in the main method

--- GitT/src/com/mbl111/test/Charne.java
+++ GitT/src/com/mbl111/test/Charne.java
@@ -2,10 +2,10 @@ package com.mbl111.test;
 
 public class Charne {
 
-	public String s = "Hey";
+	public static String s = "Hey";
 	
 	public static void main(String[] args) {
-		
+		System.out.println(s);
 	}
 	
 }
