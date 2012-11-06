6fecc0832c75fab6c6396ded827b0f7adbf7b9b9
mbl111 <matt@mbl111.com>
2012-11-06 19:04:05 +1100

    Added Jonesy Smells String

--- GitT/src/com/mbl111/test/Charne.java
+++ GitT/src/com/mbl111/test/Charne.java
@@ -3,6 +3,7 @@ package com.mbl111.test;
 public class Charne {
 
 	public static String s = "Hey You";
+	public static String lol = "Jonesy smells";
 	
 	public static void main(String[] args) {
 		System.out.println(s);
